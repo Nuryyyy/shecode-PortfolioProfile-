bbe6999941a17673104d29d81cbbc6619160caaa
Nuryyyy <a.nurymar@gmail.com>
2022-12-25 19:25:52 +0800

    aded contact and typing effect

--- Shecode website project/shecode_V2.js
+++ Shecode website project/shecode_V2.js
@@ -5,7 +5,17 @@ $(document).ready(function(){
         } else {
             $('.navbar').removeClass("sticky")
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show")
+        }else{
+            $('.scroll-up-btn').removeClass("show") 
+        }
     })
+    // slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    })
+
 
     // toggle navbar
     $('.menu-btn').click(function(){
@@ -13,3 +23,33 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active")
     })
 })
+
+// typing animation script
+
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "FullStack", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    var typed = new Typed(".typing-2", {
+        strings: ["FullStack Software Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+// $('.carousel').owlCarousel({
+//     margin: 20,
+//     loop: true,
+//     autoplayTimeOut: 2000,
+//     autoplayHoverPause: true,
+//     responsive: {
+//         0 : {
+//             items: 1,
+//             nav: false
+//         }, 600:{
+
+//         }
+//     }
+// })
